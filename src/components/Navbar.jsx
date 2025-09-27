c21427e5727f3aa2c5f916bbc834ec5f74fc203e
noorjahanmim <noorjahanmim732@gmail.com>
2025-09-27 23:12:39 +0600

    first commitsecond

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import Container from './Container';
 const Navbar = () => {
 
   return (
-    <div className='px-8 py-4 shadow-sm'>
+    <div className='px-4 md:px-8 py-8 shadow-sm'>
 
       <Container>
         <nav className="flex justify-between items-center ">
 
         <h1 className="text-xl font-bold">CS — Ticket System</h1>
-        <ul className="flex gap-6 items-center">
+        <ul className=" hidden md:flex gap-6 items-center">
           <li><a>Home</a></li>
           <li><a>FAQ</a></li>
           <li><a>Changelog</a></li>
